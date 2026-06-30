cecdf16496509a1fa2657ba4b004a467a7780995
dermi1002 <49495596+dermi1002@users.noreply.github.com>
2026-06-29 18:58:01 -0700

    start work on tas-input

---
+++ src/tas-input/inputaction.py
@@ -0,0 +1,34 @@
+import vgamepad as vg
+
+# Gamepad Labels
+
+menuStart = vg.XUSB_BUTTON.XUSB_GAMEPAD_START
+menuBack = vg.XUSB_BUTTON.XUSB_GAMEPAD_BACK
+menuGuide = vg.XUSB_BUTTON.XUSB_GAMEPAD_GUIDE
+
+dpadUp = vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_UP
+dpadDown = vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_DOWN
+dpadLeft = vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_LEFT
+dpadRight = vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_RIGHT
+
+faceA = vg.XUSB_BUTTON.XUSB_GAMEPAD_A
+faceB = vg.XUSB_BUTTON.XUSB_GAMEPAD_B
+faceX = vg.XUSB_BUTTON.XUSB_GAMEPAD_X
+faceY = vg.XUSB_BUTTON.XUSB_GAMEPAD_Y
+
+bumperLeft = vg.XUSB_BUTTON.XUSB_GAMEPAD_LEFT_SHOULDER
+bumperRight = vg.XUSB_BUTTON.XUSB_GAMEPAD_RIGHT_SHOULDER
+
+thumbButtonLeft = vg.XUSB_BUTTON.XUSB_GAMEPAD_LEFT_THUMB
+thumbButtonRight = vg.XUSB_BUTTON.XUSB_GAMEPAD_RIGHT_THUMB
+
+def initialize_gamepad():
+    gamepad = vg.VX360Gamepad()
+
+def press_button(button):
+    gamepad.press_button(button=button)
+    gamepad.update()
+
+def release_button(button):
+    gamepad.release_button(button=button)
+    gamepad.update()
